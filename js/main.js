d34ac64259373e393eb35197c7f08d33d802765f
JusaTiVi <jusa.itkonen@esedulainen.fi>
2026-05-18 13:07:13 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
+let nimet = []
+
 function pelaajamaara() {
     let maara = document.getElementById("maara").value;
+
+    document.getElementById("nimiosa").style.display = "block"
+}
+
+function pelaajat() {
+    let nimi = document.getElementById("pelaajanimet").value;
+
+    if (nimet.length >= maara) {
+        return
+    }
+
+    nimet.push(nimi)
+
+    document.getElementById("pelaajat")
 }
 
 function heita() {
