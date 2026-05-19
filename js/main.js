d5f51c717809c7bb7ede596922abe435399e45b4
JusaTiVi <jusa.itkonen@esedulainen.fi>
2026-05-19 13:09:01 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 let nimet = []
+let maara = 0
+let vuoro = 0
 
 function pelaajamaara() {
-    let maara = document.getElementById("maara").value;
+    maara = document.getElementById("maara").value;
 
     document.getElementById("nimiosa").style.display = "block"
 }
@@ -10,14 +12,37 @@ function pelaajat() {
     let nimi = document.getElementById("pelaajanimet").value;
 
     if (nimet.length >= maara) {
+        alert("kaikki pelaajat on jo lisätty")
         return
     }
 
     nimet.push(nimi)
 
-    document.getElementById("pelaajat")
+    let li = document.createElement("li")
+
+    li.textContent = nimi
+
+    document.getElementById("pelaajat").appendChild(li)
+
+    document.getElementById("pelaajanimet").value = ""
 }
 
-function heita() {
-    numero = Math.random(6)
+function seuraavavuoro() {
+    vuoro += 1
+
+    if (vuoro >= nimet.length) {
+        vuoro = 0
+    }
+}
+
+function paivitavuoro() {
+    document.getElementById("vuoro").textContent =
+    "vuorossa: " + nimet[vuoro]
+}
+
+function heitto() {
+    let noppa = Math.floor(Math.random() * 6) + 1;
+    if (numero === 1) {
+        seuraavavuoro()
+    }
 }
